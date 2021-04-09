f2f9f4d4b7852602b5cb771cb4579c6124105b15
fvdldn <f.e.vandelden@tudelft.nl>
2021-04-09 15:13:37 +0200

    added description of output

--- count-lines.py
+++ count-lines.py
@@ -1,6 +1,8 @@
 """deze module laat zien hoeveel regels input er zijn op de commandline
 lines in de standard input
-Input: any string from the system standard input"""
+Input: any string from the system standard input
+The output is a message  below the command line
+"""
 
 import sys
 
