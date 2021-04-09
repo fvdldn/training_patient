d8a0f0b2a04d975bae3a7519491c91e98b23de21
fvdldn <f.e.vandelden@tudelft.nl>
2021-04-09 13:48:36 +0200

    new directory treatments added

--- count-lines.py
+++ count-lines.py
@@ -1,5 +1,12 @@
+"""deze module laat zien hoeveel regels input er zijn op de commandline
+lines in de standard input
+Input: any string from the system standard input"""
+
 import sys
+
 count = 0
+
 for line in sys.stdin:
     count += 1
-print(count, " lines in de standaard input")
+
+print(count, "lines in de standaard input")
